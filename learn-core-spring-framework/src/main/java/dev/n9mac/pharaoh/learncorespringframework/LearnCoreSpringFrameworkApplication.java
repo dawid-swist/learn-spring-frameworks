67df13ca2882d4d170befd7ecfa1f365b85d5140
Dawid Świst <dawid.swist@tietoevry.com>
2023-11-07 14:43:42 +0100

    initial commit

---
+++ learn-core-spring-framework/src/main/java/dev/n9mac/pharaoh/learncorespringframework/LearnCoreSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package dev.n9mac.pharaoh.learncorespringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCoreSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCoreSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ learn-core-spring-framework/src/main/java/dev/n9mac/pharaoh/learncorespringframework/WebController.java
@@ -0,0 +1,15 @@
+package dev.n9mac.pharaoh.learncorespringframework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class WebController {
+
+    @GetMapping
+    public String salutation() {
+        return "Hello world!";
+    }
+}
